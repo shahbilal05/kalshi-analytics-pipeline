f786e1052eb91b9d4999edcfad4e410a0a684dc0
Bilal Shah <shahbilal5002@gmail.com>
2025-12-24 16:10:41 -0600

    pagination and appending

--- extract/extract.py
+++ extract/extract.py
@@ -1,37 +1,53 @@
 import requests
 from datetime import datetime
 
-
 def fetch_closed_events(limit=200, cursor=None):
     events_url = "https://api.elections.kalshi.com/trade-api/v2/events"
     events = []
-    cursor = None
 
     while True:
         params = {
             'status': 'closed',
             'limit': limit
         }
+
         if cursor:
             params['cursor'] = cursor
+        
         response = requests.get(events_url, params=params)
+        response.raise_for_status()
 
-        return events
+        data = response.json()
+        events.extend(data.get("events", []))
 
+        cursor = data.get("cursor")
+        if not cursor:
+            break
+
+    return events
 
 
 def fetch_closed_markets(limit=200, cursor=None):
     markets_url = "https://api.elections.kalshi.com/trade-api/v2/markets"
     markets = []
-    cursor = None
 
     while True:
         params = {
             'status': 'closed',
             'limit': limit
         }
+
         if cursor: 
             params['cursor'] = cursor
-        response = requests.get(markets_url, params=params)
 
-        return markets 
+        response = requests.get(markets_url, params=params)
+        response.raise_for_status()
+
+        data = response.json()
+        markets.extend(data.get("markets", []))
+
+        cursor = data.get("cursor")
+        if not cursor:
+            break
+
+    return markets
