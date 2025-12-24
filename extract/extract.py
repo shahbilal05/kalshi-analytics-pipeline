167461cb713d59817ced5608c24983ec5b406256
Bilal Shah <shahbilal5002@gmail.com>
2025-12-24 16:05:17 -0600

    initial events and markets functions

--- extract/extract.py
+++ extract/extract.py
@@ -1,8 +1,37 @@
 import requests
+from datetime import datetime
 
-response = requests.get(
-    "https://api.elections.kalshi.com/trade-api/v2/events",
-    params={"status": "closed", "limit": 5}
-)
 
-print(response.json())
+def fetch_closed_events(limit=200, cursor=None):
+    events_url = "https://api.elections.kalshi.com/trade-api/v2/events"
+    events = []
+    cursor = None
+
+    while True:
+        params = {
+            'status': 'closed',
+            'limit': limit
+        }
+        if cursor:
+            params['cursor'] = cursor
+        response = requests.get(events_url, params=params)
+
+        return events
+
+
+
+def fetch_closed_markets(limit=200, cursor=None):
+    markets_url = "https://api.elections.kalshi.com/trade-api/v2/markets"
+    markets = []
+    cursor = None
+
+    while True:
+        params = {
+            'status': 'closed',
+            'limit': limit
+        }
+        if cursor: 
+            params['cursor'] = cursor
+        response = requests.get(markets_url, params=params)
+
+        return markets 
