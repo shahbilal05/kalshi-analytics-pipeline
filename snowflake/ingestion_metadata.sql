2de00bb6d52d2cf2a49db4fb40207d41875ea796
Bilal Shah <shahbilal5002@gmail.com>
2026-01-07 23:37:39 -0600

    add metadata table to track pipeline

--- snowflake/ingestion_metadata.sql
+++ snowflake/ingestion_metadata.sql
@@ -1,4 +1,7 @@
-CREATE TABLE ingestion_metadata (
-    pipeline_name STRING PRIMARY KEY,
-    last_processed_ts TIMESTAMP
+CREATE TABLE IF NOT EXISTS ingestion_metadata (
+    pipeline_name VARCHAR PRIMARY KEY,
+    last_processed_ts TIMESTAMP_NTZ NOT NULL,
+    last_successful_run TIMESTAMP_NTZ,
+    records_processed NUMBER(40,0),
+    status VARCHAR(50) 
 );
